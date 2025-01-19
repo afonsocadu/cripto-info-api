094374c5ce7895b915cf557ca646a988603f7923
Carlos Afonso <afonso.cadu@gmail.com>
2025-01-19 14:56:26 +0000

    Create method create_encryption_key in user model and method create_encryption_key_for_user in encryption_key model

--- app/models/encryption_key.rb
+++ app/models/encryption_key.rb
@@ -1,5 +1,12 @@
 class EncryptionKey < ApplicationRecord
   belongs_to :user
 
+  attr_encrypted :encrypted_key, key: [ENV['ENCRYPTION_KEY']].pack('H*')
 
+  # Creates an encryption key for the user
+  def self.create_encryption_key_for_user(user)
+    key = SecureRandom.random_bytes(32)
+    encryption_key = EncryptionKey.create!(user: user, encrypted_key: key)
+    encryption_key
+  end
 end
